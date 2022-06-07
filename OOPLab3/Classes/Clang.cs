d7a12b646fe017e15329eefde018db44035a714c
schnaya <76622822+schnaya@users.noreply.github.com>
2022-06-07 11:06:12 +0300

    Update Clang.cs

--- OOPLab3/Classes/Clang.cs
+++ OOPLab3/Classes/Clang.cs
@@ -9,6 +9,6 @@ namespace Lab3.Classes
     public class Clang: Languages
     {
         public override string Move()
-            => $"{LanguageName} uses a compact notation and provides the programmer with the ability to operate with the addresses of data as well as with their values.";
+            => $"{Languagename} uses a compact notation and provides the programmer with the ability to operate with the addresses of data as well as with their values.";
     }
 }
